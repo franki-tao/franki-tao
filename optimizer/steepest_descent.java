151dc96a40e9a39ac57ec4fa94844ba2aaced6df
franki-tao <141133708+franki-tao@users.noreply.github.com>
2023-09-06 11:54:26 +0800

    Update steepest_descent.java

--- optimizer/steepest_descent.java
+++ optimizer/steepest_descent.java
@@ -1,8 +1,5 @@
-package ht_Math.Solver.optimizer;
-
-import ht_Math.Function.ht_functionN;
-import ht_Math.Matrix.h_matrix;
-import ht_Math.Solver.domain.problem;
+import Function.ht_functionN;
+import domain.problem;
 
 import java.util.Arrays;
 
@@ -50,10 +47,6 @@ public class steepest_descent extends optimize{
                 return new double[]{200*(x[1]-x[0]*x[0])*(-2*x[0])+2*(x[0]-1), 200*(x[1]-x[0]*x[0])};
             }
 
-            @Override
-            public h_matrix calHess(double[] x) {
-                return null;
-            }
         };
 
         steepest_descent optimize = new steepest_descent(x0, prob,1000,1e-5);
